8480f2f9c812703f2a8f9791b6cf550699ac95b5
P0laris0211 <99697812+P0laris0211@users.noreply.github.com>
2022-09-12 14:42:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const newYears = '1 jan 2023';
+
+function countdown() {
+   const newYearsDate = new Date(newYears);
+   const currentDate = new Date();
+
+   const totalSeconds = (newYearsDate - currentDate) / 1000;
+   const days = Math.floor(totalSeconds / 3600 / 24);
+   const hours = Math.floor(totalSeconds / 3600) % 24;
+   const minutes = Math.floor(totalSeconds / 60) % 60;
+   const seconds = Math.floor(totalSeconds) % 60;
+
+   document.getElementById('days').innerText = formatTime(days);
+   document.getElementById('hours').innerText = formatTime(hours);
+   document.getElementById('minutes').innerText = formatTime(minutes);
+   document.getElementById('seconds').innerText = formatTime(seconds);
+}
+
+function formatTime(time) {
+   return time < 10 ? '0' + time : time;
+}
+
+setInterval(countdown, 1000);
